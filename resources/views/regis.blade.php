e40097c3b9556f4b9869a85e66624e7b322682dc
Sainrahmani12 <husainrahmani127@gmail.com>
2021-11-21 17:49:08 +0700

    perbarui

--- resources/views/regis.blade.php
+++ resources/views/regis.blade.php
@@ -22,20 +22,16 @@
         <form action="{{route('posting')}}" method="post">
             @csrf
             <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nama</label>
+                <input name="nama" type="text" class="form-control" id="exampleInputEmail1">
+            </div>
+            <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input name="email" type="email" class="form-control" id="exampleInputEmail1">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input name="password" type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputCity1" class="form-label">City</label>
-                <input name="city" type="text" class="form-control" id="exampleInputCity1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
             <div class="container text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
